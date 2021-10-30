8204b5bc6ce839456d661251dd30d32bbf43d3ed
raheelbaig <68862554+raheelbaig@users.noreply.github.com>
2021-10-30 13:22:40 +0500

    push changes

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/',(req,res) => {
-    app.send("HI, How are you.../help, /contact")
+    res.send("HI, How are you.../help, /contact")
 })
 app.get('/help',(req,res) => {
-    app.send("What's problem?")
+    res.send("What's problem?")
 })
 app.get('contact',(req,res) => {
-    app.send("baig8911@gmail.com")
+    res.send("baig8911@gmail.com")
 })
 
 app.listen(port,() => {
